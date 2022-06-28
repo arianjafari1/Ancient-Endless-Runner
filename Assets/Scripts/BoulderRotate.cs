f28eebf8ce1c5abed61750ad4af0d9a65757c7dc
MallyKM <malachikm1703@gmail.com>
2022-06-28 01:50:20 +0100

    Update BoulderRotate.cs

--- Assets/Scripts/BoulderRotate.cs
+++ Assets/Scripts/BoulderRotate.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BoulderRotate : MonoBehaviour
 {
+    /// <summary>
+    /// Code and dev notes by Malachi unless otherwise specified
+    /// 27/06/22 - created boulder
+    ///          - boulder rotates
+    ///          - flipped camera to show boulder (in editor)
+    /// 
+    /// </summary>
     [SerializeField] private float rotationAmount;
     [SerializeField] private GameObject boulder;
     private void FixedUpdate()
