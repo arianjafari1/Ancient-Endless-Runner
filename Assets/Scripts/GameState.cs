1bec67587f61dd9b0f3dc0c05fbc5d47ed9f5b52
arianjafari1 <arian.jafari@outlook.com>
2022-07-12 17:08:12 +0100

    Added a Global Timer in the GameState script which is attached to the GameManager object

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    private float timer; // this is the float timer
+    private int globalTime; //this is the actual global time which I will have a getter and setter for
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime; //using delta time to count time in float
+        globalTime = (int)(timer % 60); //turning the timer to integer
+        Debug.Log(globalTime);
+    }
+
+    public int GlobalTime //getters and setters for globalTime
+    {
+        get
+        {
+            return globalTime;
+        }
+        set
+        {
+            if (value < 0)
+                globalTime = 0;
+            else
+                globalTime = value;
+        }
+    }
+
+
+
+}
