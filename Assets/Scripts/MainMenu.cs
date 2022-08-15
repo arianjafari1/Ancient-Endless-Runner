2c2f852fb7acecb7fff6c19885027ed598299eca
arianjafari1 <arian.jafari@outlook.com>
2022-08-15 17:13:32 +0100

    more stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
@@ -18,14 +19,13 @@ public class MainMenu : MonoBehaviour
     /// </summary>
 
     [SerializeField] private Slider volumeSlider;
-    private Dropdown resolutionChanger;
+    [SerializeField] private TMP_Dropdown resolutionChanger;
     private const float defaultSliderValue= 1f; //default value of the Volume slider
     private List<int> widths = new List<int>() {848, 1024, 1280, 1280, 1366, 1440, 1600, 1680, 1920, 1920, 2560, 2560, 3840, 3840, 5120, 5120, 7680, 8192 }; //list of widths that will be used for resolutions
     private List<int> heights = new List<int>() {480, 640, 720, 800, 768, 900, 900, 1050, 1080, 1200, 1400, 1600, 2160, 2400, 2880, 3200, 4320, 5120 }; //list of heights that will be used for resolutions
 
     private void Awake()
     {
-        resolutionChanger = GetComponent<Dropdown>();
         checkForPreviousSettings(); //checks for previous saved settings in player prefs
         Debug.Log (Screen.width + " X " + Screen.height);
         Debug.Log(resolutionChanger.value);
